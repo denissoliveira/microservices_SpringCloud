4c90d66627d6e2bd58c713304edf465832129c5b
denissoliveira <denissoliveira@gmail.com>
2021-05-07 21:12:00 -0300

    feat: Spring feign

--- loja/src/main/java/br/com/microservico/loja/LojaApplication.java
+++ loja/src/main/java/br/com/microservico/loja/LojaApplication.java
@@ -2,13 +2,13 @@ package br.com.microservico.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class LojaApplication {
 	
 	@Bean
--- loja/src/main/java/br/com/microservico/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservico/loja/service/CompraService.java
@@ -1,27 +1,37 @@
 package br.com.microservico.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.microservico.loja.client.FornecedorClient;
 import br.com.microservico.loja.controller.dto.CompraDTO;
 import br.com.microservico.loja.controller.dto.InfoFornecedorDTO;
-import br.com.microservico.loja.utils.Constants;
 
 @Service
 public class CompraService {
 	
+//	@Autowired
+//	private RestTemplate client;
+//	
+//	@Autowired
+//	private DiscoveryClient eurekaClient;
+	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 	
 	public void realizaCompra(CompraDTO compra) {
 		// RestTemplate client = new RestTemplate(); # não sabe resolver o serviço descoberto pelo eureka, tem que ser o gerenciado pelo spring com @loadbalance
-		ResponseEntity<InfoFornecedorDTO> exchange =
-				client.exchange(Constants.FORNECEDOR_URI+compra.getEndereco().getEstado(),
-						HttpMethod.GET, null, InfoFornecedorDTO.class);
-		System.out.println(exchange.getBody().getEndereco());
+//		ResponseEntity<InfoFornecedorDTO> exchange =
+//				client.exchange(Constants.FORNECEDOR_URI+compra.getEndereco().getEstado(),
+//						HttpMethod.GET, null, InfoFornecedorDTO.class);
+//		
+//		eurekaClient.getInstances("fornecedor").stream()
+//			.forEach(fornecedor -> {
+//				System.out.println("Localhost fornecedor: "+fornecedor.getPort());
+//			});
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		System.out.println("Estao obtido do fornecedor :"+info.getEndereco());
 	}
 
 }
